e8d32404c9b837585e10a54ae63bd4ac1b6a0459
ErikLozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-07-13 13:57:27 +0300

    Added Login
    Added Login request and  after successful validations, the user is logged in and redirected to Home Page.

--- src/views/login.js
+++ src/views/login.js
@@ -1,17 +1,18 @@
 import {html,render} from '../../node_modules/lit-html/lit-html.js';
+import page from '../../node_modules/page/page.mjs';
 
 const loginTemplate = () => html`
 <div class="loginForm" >
     <h1>Log in your account</h1>
-    <form>
+    <form @submit=${onSubmit}>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
@@ -19,6 +20,46 @@ const loginTemplate = () => html`
 `
 
 export function loginView() {
-    console.log('hi!');
     render(loginTemplate(), document.querySelector('main'));
 }
+
+
+async function onSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+
+    try {
+
+        if(email == '' || password == '') {
+            throw new Error('Please fill in all inputs!');
+        }
+
+        const response = await fetch('http://localhost:3030/users/login',{
+            method: 'POST',
+            headers: {'Content-Type': 'application.json'},
+            body: JSON.stringify({email,password})
+        })
+    
+        if(!response.ok) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+        const result = await response.json();
+        localStorage.setItem('token', result.accessToken);
+        localStorage.setItem('ownerId', result._id);
+
+        page.redirect('/');
+    } catch (error) {
+        alert(error.message)
+    }
+
+
+    console.log(result);
+}
+
+
